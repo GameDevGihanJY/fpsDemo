3b952c5689db2c39e21f40a94085183f96472510
GameDevGihanJY <135412394+GameDevGihanJY@users.noreply.github.com>
2023-07-06 23:16:26 +0530

    Player Movement
    Added gravity & Movement

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private CharacterController _cController;
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    private float _gravity = 9.81f;
+
+    void Start()
+    {
+        _cController = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        ProcessMovement();
+    }//update
+
+    private void ProcessMovement()
+    {
+        Vector3 _direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        Vector3 _velocity = _direction * _speed;
+
+        //Apply gravity
+        _velocity.y -= _gravity;
+
+        _cController.Move(_velocity * Time.deltaTime);
+    }//processMovement
+
+}//class
